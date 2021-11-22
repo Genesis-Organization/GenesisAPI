8b30524ac0c3e148cb4a40f5b9953bbc410729cd
Mateusz <mateuszslotwinski16@gmail.com>
2021-11-22 16:11:59 +0100

    Sciences are now in database

--- src/controllers/sciences.ts
+++ src/controllers/sciences.ts
@@ -1,10 +1,15 @@
 import GroupModel from '../database/models/group'
+import ScienceModel from '../database/models/science'
 import { Request, Response } from 'express'
 
 class GroupActions {
   async getGroups(req: Request, res: Response) {
     const group = await GroupModel.find({})
     res.status(200).json(group)
+  }
+  async getSciences(req: Request, res: Response) {
+    const science = await ScienceModel.find({})
+    res.status(200).json(science)
   }
 }
 
--- src/routes/shared/sciences.ts
+++ src/routes/shared/sciences.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import GroupActions from '../../controllers/sciences'
 
 const sciencesRouter: Router = Router()
-sciencesRouter.get('/groups', GroupActions.getGroups)
+sciencesRouter
+  .get('/groups', GroupActions.getGroups)
+  .get('/sciences', GroupActions.getSciences)
 
 export default sciencesRouter
