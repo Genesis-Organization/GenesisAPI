ebe0d0eaf741f35b4cbb344bd58bd1ce0e52eb96
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-06 14:54:22 +0100

    first tests

---
+++ tests/unit/endpoints/sciences/groups.spec.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import createServer from '@/server'
+
+describe('GET sciences/groups', () => {
+  test('should respond with a 200 status code', async () => {
+    const app = await createServer()
+    const response = await request(app).get('/api/sciences/groups')
+    expect(response.statusCode).toBe(200)
+  })
+  test('should specify json in the content type header', async () => {
+    const app = await createServer()
+    const response = await request(app).get('/api/sciences/groups')
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('application/json')
+    )
+  })
+  test('should response with a array of group objects', async () => {
+    const app = await createServer()
+    const response = await request(app).get('/api/sciences/groups')
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          GroupID: expect.any(Number),
+          GroupName: expect.any(String),
+        }),
+      ])
+    )
+  })
+})
---
+++ tests/unit/endpoints/sciences/sciences.spec.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import createServer from '@/server'
+
+describe('GET sciences/groups', () => {
+  test('should respond with a 200 status code', async () => {
+    const app = await createServer()
+    const response = await request(app).get('/api/sciences/sciences')
+    expect(response.statusCode).toBe(200)
+  })
+  test('should specify json in the content type header', async () => {
+    const app = await createServer()
+    const response = await request(app).get('/api/sciences/sciences')
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('application/json')
+    )
+  })
+  test('should response with a array of group objects', async () => {
+    const app = await createServer()
+    const response = await request(app).get('/api/sciences/sciences')
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          Group: expect.any(Number),
+          ScienceID: expect.any(Number),
+          ScienceName: expect.any(String),
+        }),
+      ])
+    )
+  })
+})
